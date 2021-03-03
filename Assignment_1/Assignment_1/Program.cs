991e846f9d58f3593b627ed9ea20132f8956752f
atowarrahman <emonatowarrahman@gmail\.com>
2021-03-03 13:53:43 +0600

    last commit

--- Assignment_1/Assignment_1/Program.cs
+++ Assignment_1/Assignment_1/Program.cs
@@ -11,8 +11,8 @@ namespace Assignment_1
         static void Main(string[] args)
         {
             Bank ourBank = new Bank("Our Bank");
-            Account a = new Account("Bank", 30000, 2, new Address(20, 10, "Dhaka", "Bangladesh"));
-            Account b = new Account("Bank2", 30000, 3, new Address(20, 10, "Dhaka", "Bangladesh"));
+            Account a = new Account("Atowar", 30000, 2, new Address(20, 10, "Dhaka", "Bangladesh"));
+            //Account b = new Account("Bank2", 30000, 3, new Address(20, 10, "Dhaka", "Bangladesh"));
             a.ShowAccountInformation();
             a.Deposit(500);
             a.Withdraw(100);
