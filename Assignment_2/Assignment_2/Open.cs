22af082c564cc9b23d7f7ab892362ad4dd284fff
atowarrahman <emonatowarrahman@gmail\.com>
2021-03-06 22:20:18 +0600

    assignment 2 commit

--- Assignment_2/Assignment_2/Open.cs
+++ Assignment_2/Assignment_2/Open.cs
@@ -8,5 +8,42 @@ namespace Assignment_2
 {
     class Open
     {
+        protected string name, dob, address;
+        protected double startbCus;
+        protected int accnum;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Dob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public double StartbCus
+        {
+            get { return startbCus; }
+            set { startbCus = value; }
+        }
+        public int Accnum
+        {
+            get { return accnum; }
+            set { accnum = value; }
+        }
+        public Open(string name, string dob, string address, double startbCus)
+        {
+            Name = name;
+            Dob = dob;
+            Address = address;
+            StartbCus = startbCus;
+            Accnum = accnum + GetHashCode();
+        }
     }
 }
+
